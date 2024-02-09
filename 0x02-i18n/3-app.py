f87c6d973b610f32ff10c048d57c93fa7bff7bb4
Alexus91 <mrfadlkamal@gmail.com>
2024-02-09 20:41:55 +0100

    7. Infer appropriate time zone

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Flask webapp """
 from flask import Flask, render_template, request
-from flask_babel import Babel
-
+from flask_babel import Babel, _
+import os
 
 class Config(object):
     """Configu Babel"""
@@ -10,23 +10,20 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
-
 @babel.localeselector
 def get_locale():
-    """ language match based on supported languages """
+    """Select a language translation to use based on user preferences."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
 @app.route('/', strict_slashes=False)
-def index() -> str:
+def index():
     """Handles / route"""
     return render_template('3-index.html')
 
-
 if __name__ == "__main__":
     app.run(port="5000", host="0.0.0.0", debug=True)
+
