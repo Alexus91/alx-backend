3c61fbcd231f2bf0883bcce1b8dd09cd7410bd52
root <Alexus91>
2024-02-09 18:37:23 +0000

    fixing pycodestyle

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,12 +17,14 @@ class Config:
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
     """
     language match based on supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route("/")
 def index():
