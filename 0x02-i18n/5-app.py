4dacb9bd5a2a98c07fb628a7444645f71f5c208b
root <Alexus91>
2024-02-09 18:52:31 +0000

    try a new approach

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
-"""fask web app"""
-from typing import Dict, Union
+"""
+Flask app
+"""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-app = Flask(__name__)
 
-app.url_map.strict_slashes = False
-
-
-class Config:
-    """Represents a Flask Babel configuration."""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-babel = Babel(app)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -26,40 +14,55 @@ users = {
 }
 
 
-@app.route("/")
-def index_5() -> str:
-    """The index function displays the home pagen """
-    return render_template("5-index.html")
+class Config(object):
+    """
+    Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """ Determines the best match for the client  """
-    locale = request.args.get('locale')
-    supported_languages = app.config["LANGUAGES"]
-    if locale and locale in supported_languages:
-        return locale
-    else:
-        best_match = request.accept_languages.best_match(supported_languages)
-        return best_match
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
-def get_user() -> Union[Dict, None]:
-    """Returns a user dictionary based on the id """
-    login_id = request.args.get('login_as')
-    if login_id:
-        return users.get(int(login_id))
+def get_user():
+    """Returns a user dictionary based on the id"""
+    id = request.args.get('login_as', None)
+    if id is not None and int(id) in users.keys():
+        return users.get(int(id))
     return None
 
 
 @app.before_request
-def before_request() -> None:
-    """Function to be executed before every request."""
+def before_request():
+    """
+    Function to be executed before every request
+    """
     user = get_user()
     g.user = user
 
 
-app.before_request(before_request)
+@babel.localeselector
+def get_locale():
+    """
+    Determines the best match for the client
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    The index function displays the home pagen
+    """
+    return render_template('5-index.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
