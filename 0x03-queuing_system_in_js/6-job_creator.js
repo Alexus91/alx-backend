bc320af4550dfcf3cc21b2933e518f3c04e2feae
Alexus91 <mrfadlkamal@gmail.com>
2024-03-05 19:09:54 -0800

    6. Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import { createQueue } from 'kue';
+
+// Create queue
+const queue = createQueue({name: 'push_notification_code'});
+
+// Job data
+const jobData = {
+  phoneNumber: '0707240068',
+  message: 'This is the code to verify your account'
+};
+
+// Create job
+const job = queue.create('push_notification_code', jobData);
+
+job
+  // When job is created without error, log to the console 
+  .on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+  })
+
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+
+  .on('failed', () => {
+    console.log('Notification job failed');
+  })
+  
+  .save((error) => {
+    if (error) {
+      console.log('Error creating job:', error);
+    }
+  });
